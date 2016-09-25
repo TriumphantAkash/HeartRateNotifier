59e0a7c1530c5a856969a8ad2910a2aec0e1f23c
pavan vecham <pavan vecham>
2016-09-25 14:25:59 -0500

    new files

---
+++ main/java/com/example/kumar_000/myapplication_akash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kumar_000.myapplication_akash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
